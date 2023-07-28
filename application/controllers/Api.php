ae702d0d9392c4e07ab19eb25343fe64ac1e1f57
Aldi Nurfarizha <55512360+aldinurfarizha@users.noreply.github.com>
2023-07-28 16:08:34 +0700

    update foto inventaris

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -38,6 +38,16 @@ class Api extends CI_Controller  {
     }   
     public function detailInventaris(){
         $id=$this->input->post('id');
+        $id=filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        if($id==''){
+             return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(500)
+            ->set_output(json_encode(array(
+                    'status' => False,
+                    'messages' => 'Barcode Tidak Cocok'
+            )));
+        }
         $data=get_detail_barang($id);
         $history_update=getHistoryUpdate($id)->result();
         if($data==null){
@@ -46,7 +56,7 @@ class Api extends CI_Controller  {
             ->set_status_header(500)
             ->set_output(json_encode(array(
                     'status' => False,
-                    'messages' => 'Invalid Id Aset/Tidak ditemukan !'
+                    'messages' => 'Invalid Id ! Aset Tidak ditemukan.'
             )));
         }
         return $this->output
@@ -57,6 +67,124 @@ class Api extends CI_Controller  {
                 'data'=>$data,
                 'history_update'=>$history_update
         )));
+    }
+    public function updateHistoryInventaris(){
+        $id_inventaris=$this->input->post('id_inventaris');
+        $keterangan=$this->input->post('keterangan');
+        $kondisi_baik=$this->input->post('kondisi_baik');
+        $user=$this->input->post('user');
+        $config["upload_path"]   = TEMP_PATH;
+        $config["allowed_types"] = '*';
+        $config['encrypt_name'] = TRUE;
+        $config['max_size'] = '10000';
+        $this->load->library('image_lib');
+        $conf_resize['image_library'] = 'gd2';
+        $conf_resize['maintain_ratio'] = true;
+        $conf_resize['width'] = 480;
+        $this->load->library('upload', $config);			
+        if ( ! $this->upload->do_upload("foto")) {
+            $error = array('error' => $this->upload->display_errors());
+            $response=array(
+                'message'=>$error
+            );
+            return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(400)
+            ->set_output(json_encode($response
+            ));
+        }else{
+            $data_file = array('upload_data' => $this->upload->data());
+            $foto_name= $data_file['upload_data']['file_name'];
+            $conf_resize['source_image'] = TEMP_PATH.$foto_name;
+            $conf_resize['new_image'] = FOTO_HISTORY_BARANG_PATH.$foto_name;
+            $this->image_lib->initialize($conf_resize);
+            $this->image_lib->resize();
+            if ($foto_name && file_exists(TEMP_PATH . $foto_name)) {
+                unlink(TEMP_PATH . $foto_name);
+            }
+            if($kondisi_baik=="BAIK"){
+                $kondisi=1;
+            }else{
+                $kondisi=0;
+            }
+            $data=array(
+                'foto'=>$foto_name,
+                'keterangan'=>$keterangan,
+                'kondisi_baik'=>$kondisi,
+                'id_inventaris'=>$id_inventaris,
+                'user'=>$user,
+                'date_updated'=>date('Y-m-d h:i:s')
+            );
+            $inventaris=array(
+                'kondisi_baik'=>$kondisi,
+                'last_update'=>date('Y-m-d h:i:s')
+            );
+            $inventaris_where=array(
+                'id_inventaris'=>$id_inventaris
+
+            );
+            $this->Global_model->update('inventaris',$inventaris_where,$inventaris);
+            $this->Global_model->insert('history_update', $data);
+            $response=array(
+                'message'=>"Berhasil Menambahkan history Update"
+            );
+            return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(200)
+            ->set_output(json_encode($response
+            ));
+        }
+    }
+      public function updateFotoInventaris(){
+        $id_inventaris=$this->input->post('id_inventaris');
+        $user=$this->input->post('user');
+        $config["upload_path"]   = TEMP_PATH;
+        $config["allowed_types"] = '*';
+        $config['encrypt_name'] = TRUE;
+        $config['max_size'] = '10000';
+        $this->load->library('image_lib');
+        $conf_resize['image_library'] = 'gd2';
+        $conf_resize['maintain_ratio'] = true;
+        $conf_resize['width'] = 480;
+        $this->load->library('upload', $config);			
+        if ( ! $this->upload->do_upload("foto")) {
+            $error = array('error' => $this->upload->display_errors());
+            $response=array(
+                'message'=>$error
+            );
+            return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(400)
+            ->set_output(json_encode($response
+            ));
+        }else{
+            $data_file = array('upload_data' => $this->upload->data());
+            $foto_name= $data_file['upload_data']['file_name'];
+            $conf_resize['source_image'] = TEMP_PATH.$foto_name;
+            $conf_resize['new_image'] = FOTO_BARANG_PATH.$foto_name;
+            $this->image_lib->initialize($conf_resize);
+            $this->image_lib->resize();
+            if ($foto_name && file_exists(TEMP_PATH . $foto_name)) {
+                unlink(TEMP_PATH . $foto_name);
+            }
+            $data=array(
+                'foto_barang'=>$foto_name,
+                'admin'=>$user,
+                'last_update'=>date('Y-m-d h:i:s')
+            );
+            $where=array(
+                'id_inventaris'=>$id_inventaris
+            );
+            $this->Global_model->update('inventaris',$where,$data);
+            $response=array(
+                'message'=>"Berhasil Ubah gambar"
+            );
+            return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(200)
+            ->set_output(json_encode($response
+            ));
+        }
     }
 }
 
--- application/models/Global_model.php
+++ application/models/Global_model.php
@@ -79,20 +79,20 @@ class Global_model extends CI_Model{
       return $this->db->get();
     }
     function get_detail_inventaris($id){
-      $this->db->select('inventaris.*, master_barang.*, office.*, office.nama as nama_kantor, sub_office.nama as nama_sub_kantor,master_perkiraan.*, master_perkiraan_dasar.*');
+      $this->db->select('inventaris.*, master_barang.*, office.*, office.nama as nama_kantor, sub_office.nama as nama_sub_kantor,master_perkiraan.*, master_perkiraan_dasar.*, master_ruangan_kir.nama_ruangan');
       $this->db->from('inventaris');
       $this->db->join('master_barang','master_barang.id_barang=inventaris.id_barang');
       $this->db->join('master_perkiraan','master_barang.id_perkiraan=master_perkiraan.id_perkiraan');
       $this->db->join('master_perkiraan_dasar', 'master_perkiraan.kd_perkiraan_dasar = master_perkiraan_dasar.kd_perkiraan_dasar');
       $this->db->join('office','office.of_id=inventaris.of_id');
       $this->db->join('sub_office','sub_office.sub_id=inventaris.sub_id','left');
+      $this->db->join('master_ruangan_kir','inventaris.id_ruangan_kir=master_ruangan_kir.id_ruangan_kir', 'left');
       $this->db->where(['id_inventaris'=>$id]);
       return $this->db->get();
     }
     function get_history($id){
-      $this->db->select('history_update.*, office.*');
+      $this->db->select('history_update.*');
       $this->db->from('history_update');
-      $this->db->join('office', 'history_update.of_id = office.of_id');
       $this->db->where(['id_inventaris'=>$id]);
       return $this->db->get();
     }
